dea9dafbfa5096802d8803366f796cda9d234abc
dIVCREATION <147695219+dIVCREATION@users.noreply.github.com>
2023-11-05 16:04:42 +0530

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,11 @@
+const Colors = {
+
+    dark : '#rgb(70,70,70)',
+    black : '#000',
+    white : '#fff',
+    red : '#f96163',
+    dark : '#rgb(25,25,25)',
+    yellow : '#rgb(250,230,50)'
+
+}
+export default Colors;
